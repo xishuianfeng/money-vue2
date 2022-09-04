017aa3d6f18d626428e41ea4745d47e58f375505
xsaf <1767406957@qq.com>
2022-09-04 16:33:28 +0800

    创建<Icon/>

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
+const path = require('path')
+
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, 'src/assets/icons')
+
+    config.module
+      .rule('svg-sprite')
+      .test(/\.svg$/)
+      .include.add(dir).end() //包含 icons 目录
+      .use('svg-sprite-loader').loader('svg-sprite-loader').options({ extract: false })
+      .end()
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{ plainSprite: true }])
+    config.module.rule('svg').exclude.add(dir)
+  }
 }
