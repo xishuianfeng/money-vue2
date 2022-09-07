50bea83fd1eb623d9bb048385d657f78cf1b20eb
xsaf <1767406957@qq.com>
2022-09-08 00:07:11 +0800

    模块化Money.vue

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <label class="note">
+            <span class="name">备注</span>
+            <input type="text" placeholder="在这里输入备注">
+        </label>
+    </div>
+
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style lang="scss">
+    .note{
+        font-size: 14px;
+        background: #f5f5f5;
+        display: block;
+        padding-left: 0 16px;
+        display: flex;
+        align-items: center;
+        .name{
+        padding-right: 16px;
+      }
+    input{
+        height: 64px;
+        flex-grow: 1;
+        background: transparent;
+        border: none;
+        padding: 16px;
+      }
+    }
+</style>
---
+++ src/components/Money/NumberPad.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="numberPad">
+        <div class="output">100</div>
+        <div class="buttons">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>删除</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>清空</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button class="ok">OK</button>
+            <button class="zero">0</button>
+            <button>.</button>
+        </div>
+    </div>  
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style lang="scss">
+    @import '@/assets/style/help.scss';
+    .numberPad{
+      .output{
+        @extend %clearfix;
+        @extend %innerShadow;
+        font-size: 36px;
+        font-family: Consolas,monospace;
+        padding: 9px 16px;
+        text-align: right;
+        
+      }
+      .buttons{
+        @extend %clearfix;
+        > button{
+          font-family: $font-hei;
+          float: left;
+          width: 25%;
+          height: 64px;
+          background: transparent;
+          border: none;
+          &.ok{
+            height: 64*2px;
+            float: right;
+          }
+          &.zero{
+            width: 25*2%;
+          }
+          $bc:#F0F8FF;
+          &:nth-child(1){
+            background-color: $bc;
+          }
+          &:nth-child(2),&:nth-child(5){
+            background-color: darken($bc, 5%);
+          }
+          &:nth-child(3),&:nth-child(6),&:nth-child(9){
+            background-color: darken($bc, 5%*2);
+          }
+          &:nth-child(4),&:nth-child(7),&:nth-child(10){
+            background-color: darken($bc, 5%*3);
+          }
+          &:nth-child(8),&:nth-child(11),&:nth-child(13){
+            background-color: darken($bc, 5%*4);
+          }
+          &:nth-child(14){
+            background-color: darken($bc, 5%*5);
+          }
+          &:nth-child(12){
+            background-color: darken($bc, 5%*6);
+          }
+        }
+      }
+    }
+</style>
