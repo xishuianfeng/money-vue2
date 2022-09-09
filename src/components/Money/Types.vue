ba2202da11798f204b27466ad5bdac76be6a1911
xsaf <1767406957@qq.com>
2022-09-09 14:34:41 +0800

    完成Type组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -7,27 +7,21 @@
     </div>   
 </template>
 
-<script lang="js">
-export default {
-  name:'Types',
-  props:['xxx'],
-  data(){
-    return{
-      type:'-' //支出用'-'，收入用'+'
-    }
-  },
-  mounted(){
-    console.log(this.xxx);
-  },
-  methods:{
-    selectType(type){
-      // if(type !== '-' && type !== '+'){
-      //   throw new Error('type is unknown')
-      // };
-      this.type=type
+<script lang="ts">
+  import Vue from 'vue'
+  import {Component,Prop} from 'vue-property-decorator';
+
+  @Component
+  export default class Types extends Vue{
+    type = '-'; //支出用'-'，收入用'+'
+    @Prop(Number) xxx:number|undefined;
+    selectType(type:string){
+        // if(type !== '-' && type !== '+'){
+        //   throw new Error('type is unknown')
+        // };
+      this.type=type;
     }
   }
-}
 </script>
 
 <style lang="scss">
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,7 @@
   <div>
     <Layout class-prefix="layout">
       <NumberPad/>
-      <Types xxx="hi"/>
+      <Types/>
       <Notes/>
       <Tags/>
     </Layout>
@@ -10,7 +10,7 @@
 </template>
 
 <script lang="ts">
-  import NumberPad from '@/components/Money/NumberPad.vue';
+  import NumberPad from '../components/Money/NumberPad.vue';
   import Types from '../components/Money/Types.vue';
   import Notes from '../components/Money/Notes.vue';
   import Tags from '../components/Money/Tags.vue';
