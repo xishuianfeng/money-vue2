cb3141470306919e7c297058746efb179c526564
xsaf <1767406957@qq.com>
2022-09-14 00:05:44 +0800

    新增EditLabel组件

--- src/views/EditLabels.vue
+++ src/views/EditLabels.vue
@@ -5,13 +5,24 @@
 </template>
 
 <script lang="ts">
+  import tagListModel from '@/models/tagListModel';
   import Vue from 'vue';
   import { Component } from 'vue-property-decorator';
 
   @Component
   export default class Label extends Vue{
-
+    create(){
+      const id = this.$route.params.id;
+      tagListModel.fetch();
+      const tags = tagListModel.data;
+      const tag = tags.filter(t => t.id === id)[0];
+      if(tag){
+        console.log(tag);
+      }else{
+        this.$router.replace('/404')
+      }
     }
+  }
 </script>
 
 <style lang="scss" scoped>
