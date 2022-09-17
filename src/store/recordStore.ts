9367e260414c6ea254f14f9f5047f9ce264b3d95
xsaf <1767406957@qq.com>
2022-09-17 14:53:34 +0800

    模块化store 删除models

---
+++ src/store/recordStore.ts
@@ -0,0 +1,27 @@
+import clone from "@/lib/clone";
+
+const localStorageKeyName = 'recordList'
+
+let data:RecordItem[] | undefined = undefined;
+
+const recordStore = {
+  recordList : [] as RecordItem[],
+  fetchRecords(){
+    this.recordList =  JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.recordList
+  },
+  saveRecords(){
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.recordList))
+  },
+  createRecord(record:RecordItem){
+    const record2 : RecordItem = clone(record);
+    record2.createAt = new Date();
+    this.recordList?.push(record2);
+    // 可选链语法
+    recordStore.saveRecords()
+  }
+}
+
+recordStore.fetchRecords();
+
+export default recordStore;
