9914547cbfbac7d88a43983b7711e34ff4a36276
xsaf <1767406957@qq.com>
2022-09-21 00:47:56 +0800

    Labels.vue 使用 vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
   <div class="news">
-    <button @click="create">新增标签</button>
+    <button @click="createTag">新增标签</button>
   </div>
   <ul class="current">
     <li v-for="tag in tagList" :key="tag.id"
@@ -13,7 +13,9 @@
 </template>
 
 <script lang="ts">
+  import TagHelper from '@/mixins/TagHelper';
   import Vue from 'vue';
+  import { mixins } from 'vue-class-component';
   import { Component, Prop } from 'vue-property-decorator';
 
   @Component({
@@ -23,7 +25,7 @@
       }
     }
   })
-  export default class Tags extends Vue{
+  export default class Tags extends mixins(TagHelper){
     selectedTags :string[] = [];
 
     created(){
@@ -38,11 +40,6 @@
       }
       this.$emit('update:value',this.selectedTags)
     };
-    create(){
-      const name = window.prompt('请输入标签名');
-      if(!name){return window.alert('标签名不能为空');}
-      this.$store.commit('createTag',name);
-    }
   }
   </script>
 
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -18,18 +18,21 @@
   import Button from '@/components/Money/Botton.vue'
   import Vue from 'vue';
   import { Component } from 'vue-property-decorator';
+  import TagHelper from '@/mixins/TagHelper'
+  import { mixins } from 'vue-class-component';
 
   @Component({
-    components: { Button }
-  })
-  export default class Label extends Vue{
-    tags = [] // store.tagList;
-
-    createTag(){
-      const name = window.prompt('请输入标签名')
-      if(name){
-        // store.createTag(name)
+    components: { Button },
+    computed:{
+      tags(){
+        return this.$store.state.tagList;
       }
+    }
+  })
+
+  export default class Label extends mixins(TagHelper){
+    created(){
+      this.$store.commit('fatchTags');
     };
   }
 </script>
