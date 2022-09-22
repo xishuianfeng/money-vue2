2541c534081ec853e77b7219f13b2f6a7dd77b85
xsaf <1767406957@qq.com>
2022-09-22 14:19:48 +0800

    抽离Type.vue在Statistics中使用

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <ul class="type">
-            <li :class="value === '-' && 'selected'" @click = "selectType('-')">支出</li>
-            <li :class="value === '+' && 'selected'" @click = "selectType('+')">收入</li>
+            <li :class="{[classPrefix+'-item'] : classPrefix,selected: value === '-' && 'selected'}" @click = "selectType('-')">支出</li>
+            <li :class="{[classPrefix+'-item'] : classPrefix,selected: value === '+' && 'selected'}" @click = "selectType('+')">收入</li>
         </ul>
     </div>   
 </template>
@@ -13,7 +13,8 @@
   //支出用'-'，收入用'+'
   @Component
   export default class Types extends Vue{
-    @Prop() readonly value!:string;
+    @Prop(String) readonly value!:string;
+    @Prop(String) classPrefix?:string;
 
     selectType(type:string){
         // if(type !== '-' && type !== '+'){
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,17 +1,47 @@
 <template>
-  <div>
     <Layout>
-      <p>Statistics.vue</p>
-    </Layout>
-  </div>
+      <Tabs classPrefix="type" :dataSource="typeList" :value.sync="type" />
+      <Tabs classPrefix="interval" :dataSource="intervalList" :value.sync="interval"/>
+      <div>
+        type:{{type}}
+        <br/>
+        interval:{{interval}}
+      </div>
+  </Layout>
 </template>
 
 <script lang="ts">
-  export default {
-    name: "Statistics",
-}
+import Vue from "vue";
+import Types from "../components/Money/Types.vue";
+import { Component } from "vue-property-decorator";
+import Tabs from "../components/Tabs.vue";
+
+@Component({
+    components: { Types,Tabs },
+})
+  export default class Statistics extends Vue {
+    type = '-';
+    interval = 'day';
+    intervalList = [
+      {text:'按天',value:'day'},
+      {text:'按周',value:'week'},
+      {text:'按月',value:'month'}
+    ];
+    typeList = [
+      {text:'支出',value:'-'},
+      {text:'收入',value:'+'},
+    ]
+  }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  ::v-deep .type-tabs-item{
+    background-color: #fff;
+    &.selected{
+      background: #c4c4c4;
+      &::after{
+        display: none;
+      }
+    }
+  }
 </style>
